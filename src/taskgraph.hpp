5674cda51f4fbc0592f78a1b1263f5877840cf0d
Sam <samchenyu@gmail.com>
2026-09-17 23:15:29 +0100

    Fleshing out mechanics

--- src/taskgraph.hpp
+++ src/taskgraph.hpp
@@ -2,6 +2,7 @@
 
 #include "threadpool.hpp"
 #include <queue>
+#include <utility>
 #include <vector>
 #include <future>
 #include <functional>
@@ -23,14 +24,10 @@
  *      these callbacks will fire to the dependent future_wrapper 
  *          these dependent future_wrappers will check if all the dependents are sati
  *          satisified, and if it is, then it will be enqueued to the internal threadpool
- *
- *
- *
+
  *      so a bit more formally:
  *
  *      struct task_wrapper {
- *          
- *              
  *          std::package_task<ReturnType()> task;
  *          std::vector<std;:function()>> callbacks;
  *
@@ -40,9 +37,6 @@
  *          void ready_dependent();
  *
  *      }
- *
- *
- *
  *      struct future_wrapper {
  *
  *          std::future val;
@@ -71,11 +65,62 @@
  *           they will be heap allocated, and the upstream tasks will hold their pointers
  *           if they have all requirements satisifed, the task will enqeueu them onto the ready_task queue
  *           the lifetime keeps them alive
- *
- *
+
  *  because the approach is using an internal threadpool, there are some performance implications: the function pointers get double wrapped - first the taskgraph's layer firing callbacks, then after that the threadpool's internal wrapping. If there ever are performance concerns then it's possible to just rewrite the threadpool's internals and it it to the taskgraph
  */
 
 class taskgraph {
+private:
+    struct task_wrapper {
+        explicit task_wrapper(const std::function<void()>& f, std::queue<task_wrapper>& ready_tasks) : m_task(std::move(f)), m_ready_tasks{ready_tasks}, m_dependency_count{0}, m_ready_dependencies{0} {
+        }
+        std::function<void()> m_task;
+        std::vector<std::function<void()>> m_callbacks;
+        std::queue<task_wrapper>& m_ready_tasks;
+
+        const int m_dependency_count;
+        int m_ready_dependencies;
+
+        void ready_dependent() const {
+            if (m_ready_dependencies == m_dependency_count) {
+                for (const auto& f : m_callbacks) {
+                    f();
+                }
+            }
+        };
+    };
+
+    template<class T>
+    struct future_wrapper {
+        explicit future_wrapper(const std::future<T>& fut, const task_wrapper* ptr) : m_fut{fut}, m_ptr{ptr} {
+
+        }
+        const std::future<T> m_fut;
+        const task_wrapper *m_ptr;
+    };
+
+    threadpool m_tp;
+    bool m_stop;
+
+    inline auto write_task(const std::function<void()>& fn) {
+        m_tp.submit // this needs to be submitted somehow
+    }
+
+public:
+    explicit taskgraph(const int& n) : m_tp{n}, m_stop{false} {}
+
+    template<typename Function, typename... Args>
+    [[nodiscard]]
+    auto submit(Function &&F, Args  &&...ArgList) {
+        // will need to add mutexes to this in the future
+        if (m_stop) {
+            throw std::runtime_error{"taskgraph::submit() after shutdown called"};
+        }
+
+        task_wrapper task{}; // wrap the function, then how do you submit it?
+
+        auto future = m_tp.submit(std::forward<Function>(F), std::forward<Args>(ArgList)...);
+        return future_wrapper{std::move(future), this};
+    }
 
 };
